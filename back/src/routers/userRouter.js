6c70c7fa060a46fcea248244a12ff9a939888f2d
Eddy Ding <edwarding04@gmail.com>
2023-07-31 15:19:20 -0400

    Add protection from cors on user post request

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import User from '../models/user.js';
 import bcrypt from 'bcrypt';
+import cors from 'cors';
 import 'express-async-errors';
 import PasswordValidator from 'password-validator';
 
 const userRouter = express.Router();
 
 const passwordSchema = new PasswordValidator();
+
+const noCors = {
+  origin: true
+};
 
 passwordSchema
   .is().min(6, 'Password must have minimum of 6 characters')
@@ -15,7 +20,7 @@ passwordSchema
   .has().digits(1, 'Password must contain a digit')
   .has().not().spaces();
 
-userRouter.post('/', async (request, response) => {
+userRouter.post('/', cors(noCors), async (request, response) => {
   const { username, email, password } = request.body;
   const passErrors = passwordSchema.validate(password, { details: true });
   if (passErrors.length >= 1) {
