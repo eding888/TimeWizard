2e912c2cbc3fc577729e72863d7186992b32a5bf
Eddy Ding <edwarding04@gmail.com>
2023-07-31 13:46:29 -0400

    Remove typescript from backend because my brain hurts

---
+++ back/src/app.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import cors from 'cors';
+import mongoose from 'mongoose';
+import nodemon
+const app = express();
+
---
+++ back/src/utils/config.ts
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGO_URL
+  : process.env.MONGO_URL;
+
+export default { PORT, MONGO_URL }
